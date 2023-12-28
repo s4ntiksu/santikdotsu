f071a46f847f19b9cd3eb1c000b1dd3aa14222ba
Alexander Timush <santik.game@yandex.ru>
2023-12-28 09:54:43 +0400

    xd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import snow from './stickers/snowflake.json';
 export default function App() {
   return (
     <div>
-                      <h1 className="absolute top-[10%] left-1/2 transform w-64 -translate-x-1/2 font-bold text-center text-4xl font-google text-[#c9ee9e5e]">santiksu | βeta</h1>
+                      <h1 className="absolute top-16 left-1/2 transform w-64 -translate-x-1/2 font-bold text-center text-4xl font-google text-[#c9ee9e5e]">santiksu | βeta</h1>
         <figure id="fade-in" className="bg-[#23261E] rounded-3xl max-sm:mt-48 p-5 h-auto w-80 mx-auto mt-48 items-center">
         <div className="flex p-5 items-center justify-center mx-auto">
                         <h1 className="text-[#C9EE9E] font-bold font-google text-center text-7xl">2</h1>
