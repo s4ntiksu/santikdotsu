04e1c6189393e3784c8af5d40a5d4b06751d51a2
s4ntiksu <santik.game@yandex.ru>
2024-01-05 17:15:39 +0400

    Adding Header (In progress)

---
+++ src/elements/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function Header() {
+  return(
+    <div>
+    <div className="fixed top-0 w-full h-16 bg-black p-5"><h2 className="text-white font-google font-bold text-xl">Home</h2>
+    </div>
+    </div>
+    )
+}
